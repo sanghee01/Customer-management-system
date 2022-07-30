c1a4a71589bf31b98029a5cd4aa4dab7fc162c48
sanghee01 <sang6299@naver.com>
2022-07-30 16:01:38 +0900

    고객 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import React, { Component } from "react";
 import Customer from "./components/Customer";
 import "./App.css";
 
-const customer = {
-  name: "이상희",
-  birthday: "010816",
-  gender: "여자",
-  job: "대학생",
-};
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "이상희",
+    birthday: "010816",
+    gender: "여자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "홍길동",
+    birthday: "040227",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "이순신",
+    birthday: "990105",
+    gender: "여자",
+    job: "직장인",
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
